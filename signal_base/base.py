fcd651fe518f57ce62d474d459e96a48986139c0
liukang <745022382@qq.com>
2018-03-27 17:00:51 +0800

    1.增加 signal基础代码。

---
+++ signal_base/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = "liukang"
+
+import signal
+import time
+import sys
+import os
+
+def handle_int(sig, frame):
+    print "get signal: %s, I will quit"%sig
+    sys.exit(0)
+
+def handle_hup(sig, frame):
+    print "get signal: %s"%sig
+
+
+if __name__ == "__main__":
+    signal.signal(2, handle_int)
+    signal.signal(1, handle_hup)
+    print "My pid is %s"%os.getpid()
+    while True:
+        time.sleep(3)
